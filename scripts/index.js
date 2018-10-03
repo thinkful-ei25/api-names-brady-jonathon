cbe704cd0e73fbcf4072d584ee5d6b63dea7a3fc
Reldalk <Reldalk@gmail.com>
2018-10-03 11:17:44 -0400

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
 /* global shoppingList, store */
+'use strict';
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
+
+
+api.getItems(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
 
 store.items.push(Item.create('apples'));
